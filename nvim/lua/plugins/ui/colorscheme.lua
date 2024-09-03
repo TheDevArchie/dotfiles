d2ec0b873ae80d7fe80836505f21d13e0cf7cd41
Spencer Archibald <spicyspice.dev@gmail.com>
2024-09-02 20:42:41 -0500

    theme update

--- nvim/lua/plugins/ui/colorscheme.lua
+++ nvim/lua/plugins/ui/colorscheme.lua
@@ -12,13 +12,23 @@ return {
         lazy = false,
         priority = 1000,
         opts = {},
-        config = function ()
-            require("flow").setup_options {
-                transparent = false,
-                fluo_color = "orange",
-                mode = "normal",
+        config = function()
+            require("flow").setup{
+                transparent = false, -- Set transparent background.
+                fluo_color = "orange", --  Fluo color: pink, yellow, orange, or green.
+                mode = "normal", -- Intensity of the palette: normal, bright, desaturate, or dark. Notice that dark is ugly!
+                aggressive_spell = false, -- Display colors for spell check.
             }
-        vim.cmd "colorscheme flow"
-        end
+            -- vim.cmd "colorscheme flow"
+        end,
     },
+    {
+      "eldritch-theme/eldritch.nvim",
+      lazy = false,
+      priority = 1000,
+      opts = {},
+      config = function()
+          vim.cmd "colorscheme eldritch"
+      end,
+    }
 }
