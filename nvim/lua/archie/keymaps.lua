12b85ef747e43aebb560803bd9e4b145ea9da3a4
Spencer Archibald <spicyspice.dev@gmail.com>
2024-09-12 00:19:36 -0500

    Fixing some keymaps

--- nvim/lua/archie/keymaps.lua
+++ nvim/lua/archie/keymaps.lua
@@ -1,18 +1,23 @@
-local keymap = vim.keymap
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
+local remap = vim.keymap.set
+
+remap('n', '<Esc>', function()
+    vim.cmd('nohlsearch')
+    vim.cmd('echon')
+end)
 
 -- Normal
-keymap.set("n", "<leader>pv", vim.cmd.Ex)
+remap("n", "<leader>w", "<cmd>w<CR>", { desc = "Write" })
+remap("n", "<leader>q", "<cmd>q<CR>", { desc = "Quit" })
+remap("n", "<leader>Q", "<cmd>qa!<CR>", { desc = "Quit all" })
+remap("n", "<leader>wq", "<cmd>wq!<CR>", { desc = "Write and Quit all" })
 
-keymap.set("n", "<leader>w", "<cmd>w<CR>", { desc = "Write" })
-keymap.set("n", "<leader>q", "<cmd>q<CR>", { desc = "Quit" })
-keymap.set("n", "<leader>Q", "<cmd>qa!<CR>", { desc = "Quit all" })
-keymap.set("n", "<leader>wq", "<cmd>wq!<CR>", { desc = "Write and Quit all" })
+remap("n", "<leader>o", "o<Esc>", { desc = "New Line Below" })
+remap("n", "<leader>O", "O<Esc>", { desc = "New Line Above" })
 
-keymap.set("n", "<leader>o", "o<Esc>", { desc = "New Line Below" })
-keymap.set("n", "<leader>O", "O<Esc>", { desc = "New Line Above" })
-
-keymap.set("n", "<leader>so", "<cmd>so %<CR>", { desc = "Shout Out File" })
+remap("n", "<leader>so", "<cmd>so %<CR>", { desc = "Shout Out File" })
 
 -- Insert
-keymap.set("i", "jj", "<Esc>") 
+remap("i", "jj", "<Esc>")
