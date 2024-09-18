dbe47f64ad9ba9d963ecc4ee43a6beffe04ff03a
Spencer Archibald <spicyspice.dev@gmail.com>
2024-09-17 21:12:42 -0500

    mini-basics Lazy -> mini.deps

--- nvim/lua/plugins/mini-basics.lua
+++ nvim/lua/plugins/mini-basics.lua
@@ -1,18 +1,13 @@
-return {
-    "echasnovski/mini.basics",
-    confic = function()
-        require("mini.basics").setup({
-            options = {
-                basic = true,
-                win_borders = "single",
-                extra_ui = true,
-            },
-            mappings = {
-                basic = false,
-            },
-            autocommands = {
-                basic = false,
-            },
-        })
-    end,
-}
+require("mini.basics").setup({
+    options = {
+        basic = true,
+        win_borders = "single",
+        extra_ui = true,
+    },
+    mappings = {
+        basic = false,
+    },
+    autocommands = {
+        basic = false,
+    },
+})
