72927d3c25e84a919db7e83916aa9843d69cf1b4
Spencer Archibald <spicyspice.dev@gmail.com>
2024-09-17 21:15:45 -0500

    Ducks stay together: Lazy -> mini.deps

--- nvim/lua/plugins/duck.lua
+++ nvim/lua/plugins/duck.lua
@@ -1,7 +1,2 @@
-return {
-    "tamton-aquib/duck.nvim",
-    config = function()
-        vim.keymap.set("n", "<leader>dd", function() require("duck").hatch("🦖", 5) end, {})
-        vim.keymap.set("n", "<leader>dk", function() require("duck").cook_all() end, {})
-    end
-}
+vim.keymap.set("n", "<leader>dd", function() require("duck").hatch("🦖", 5) end, {})
+vim.keymap.set("n", "<leader>dk", function() require("duck").cook_all() end, {})
