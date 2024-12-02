bf1988927545c5adda55b1c341b4809782d3cbcc
Spencer Archibald <spicyspice.dev@gmail.com>
2024-12-01 22:37:14 -0600

    nvim -> spaceduck theme

--- nvim/lua/plugins/ui/colorscheme.lua
+++ nvim/lua/plugins/ui/colorscheme.lua
@@ -1,8 +1,8 @@
-require("flow").setup({
-    transparent = false, -- Set transparent background.
-    fluo_color = "orange", --  Fluo color: pink, yellow, orange, or green.
-    mode = "normal", -- Intensity of the palette: normal, bright, desaturate, or dark. Notice that dark is ugly!
-    aggressive_spell = false, -- Display colors for spell check.
-})
+-- require("flow").setup({
+--     transparent = false, -- Set transparent background.
+--     fluo_color = "orange", --  Fluo color: pink, yellow, orange, or green.
+--     mode = "normal", -- Intensity of the palette: normal, bright, desaturate, or dark. Notice that dark is ugly!
+--     aggressive_spell = false, -- Display colors for spell check.
+-- })
 
-vim.cmd('colorscheme eldritch')
+vim.cmd('colorscheme spaceduck')
--- nvim/lua/plugins/ui/lualine.lua
+++ nvim/lua/plugins/ui/lualine.lua
@@ -1,39 +1,39 @@
 -- stylua: ignore
-local colors = {
-  blue   = "#00BFFF",
-  cyan   = '#79dac8',
-  green  = "#1AFFA3",
-  black  = '#080808',
-  white  = '#c6c6c6',
-  red    = '#ff5189',
-  purple = '#7600bc',
-  orange = "#FF7431",
-  dark_orange = 'FF4D00',
-  violet = '#d183e8',
-  grey   = '#303030',
-}
+-- local colors = {
+--   blue   = "#00BFFF",
+--   cyan   = '#79dac8',
+--   green  = "#1AFFA3",
+--   black  = '#080808',
+--   white  = '#c6c6c6',
+--   red    = '#ff5189',
+--   purple = '#7600bc',
+--   orange = "#FF7431",
+--   dark_orange = 'FF4D00',
+--   violet = '#d183e8',
+--   grey   = '#303030',
+-- }
 
-local bubbles_theme = {
-  normal = {
-    a = { fg = colors.black, bg = colors.orange },
-    b = { fg = colors.dark_orange, bg = colors.white },
-    c = { fg = colors.white },
-  },
-
-  insert = { a = { fg = colors.black, bg = colors.blue } },
-  visual = { a = { fg = colors.black, bg = colors.green } },
-  replace = { a = { fg = colors.black, bg = colors.red } },
-
-  inactive = {
-    a = { fg = colors.white, bg = colors.black },
-    b = { fg = colors.white, bg = colors.black },
-    c = { fg = colors.white },
-  },
-}
+-- local bubbles_theme = {
+--   normal = {
+--     a = { fg = colors.black, bg = colors.orange },
+--     b = { fg = colors.dark_orange, bg = colors.white },
+--     c = { fg = colors.white },
+--   },
+--
+--   insert = { a = { fg = colors.black, bg = colors.blue } },
+--   visual = { a = { fg = colors.black, bg = colors.green } },
+--   replace = { a = { fg = colors.black, bg = colors.red } },
+--
+--   inactive = {
+--     a = { fg = colors.white, bg = colors.black },
+--     b = { fg = colors.white, bg = colors.black },
+--     c = { fg = colors.white },
+--   },
+-- }
 
 require('lualine').setup {
   options = {
-    theme = bubbles_theme,
+    theme = 'spaceduck',
     component_separators = '',
     section_separators = { left = '', right = '' },
   },
