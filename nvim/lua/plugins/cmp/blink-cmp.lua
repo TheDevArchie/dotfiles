ecabb205c69d41b695a74851a017b8b460b258fa
Spencer Archibald <spencer.archibald@autoipacket.com>
2025-01-04 16:52:48 -0600

    experiment with colorful-menu

--- nvim/lua/plugins/cmp/blink-cmp.lua
+++ nvim/lua/plugins/cmp/blink-cmp.lua
@@ -1,6 +1,6 @@
 require('blink.cmp').setup({
   keymap = {
-    preset = 'enter'
+    preset = 'enter',
   },
   completion = {
     list = {
@@ -15,6 +15,38 @@ require('blink.cmp').setup({
       max_height = 5,
       scrolloff = 3,
       draw = {
+        components = {
+          label = {
+            width = { fill = true, max = 60 },
+            text = function(ctx)
+              local highlights_info =
+              require("colorful-menu").highlights(ctx.item, vim.bo.filetype)
+              if highlights_info ~= nil then
+                return highlights_info.text
+              else
+                return ctx.label
+              end
+            end,
+            highlight = function(ctx)
+              local highlights_info =
+              require("colorful-menu").highlights(ctx.item, vim.bo.filetype)
+              local highlights = {}
+              if highlights_info ~= nil then
+                for _, info in ipairs(highlights_info.highlights) do
+                  table.insert(highlights, {
+                    info.range[1],
+                    info.range[2],
+                    group = ctx.deprecated and "BlinkCmpLabelDeprecated" or info[1],
+                  })
+                end
+              end
+              for _, idx in ipairs(ctx.label_matched_indices) do
+                table.insert(highlights, { idx, idx + 1, group = "BlinkCmpLabelMatch" })
+              end
+              return highlights
+            end,
+          },
+        },
         columns = {
           { 'label', 'label_description', gap = 1 },
           { 'kind_icon', 'kind', gap = 1 },
