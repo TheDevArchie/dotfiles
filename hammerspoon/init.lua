3e603334bd75840b4ccd45fd6e12b45721a69c3b
Spencer Archibald <spencer.archibald@autoipacket.com>
2025-01-28 18:14:58 -0600

    Utilizing raycast instead of Hammerspoon

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,21 +1,23 @@
+hs.loadSpoon("EmmyLua")
+
 -- Application Switcher
-leader = "alt"
+-- leader = "alt"
 
-for key, name in pairs({
-    ["1"] = "Spotify",
-    ["2"] = "Wezterm",
-    ["3"] = "Slack",
-    ["4"] = "Linear",
-    ["5"] = "Obsidian",
-    ["6"] = "Zen Browser",
-    ["7"] = "Proxyman",
-
-}) do
-    hs.hotkey.bind(leader, key, function()
-        hs.alert.show("Switching to " .. name)  -- Alert to confirm the keybind is working
-        hs.application.launchOrFocus(name)
-    end)
-end
+-- for key, name in pairs({
+--     ["1"] = "Spotify",
+--     ["2"] = "Wezterm",
+--     ["3"] = "Slack",
+--     ["4"] = "Linear",
+--     ["5"] = "Obsidian",
+--     ["6"] = "Zen Browser",
+--     ["7"] = "Proxyman",
+--
+-- }) do
+--     hs.hotkey.bind(leader, key, function()
+--         hs.alert.show("Switching to " .. name)  -- Alert to confirm the keybind is working
+--         hs.application.launchOrFocus(name)
+--     end)
+-- end
 
 
 hs.notify.show("Config Settings", "", "Config Initialized!")
