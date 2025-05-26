448ad057c98a716f78bfe50b1a8eb8e44c0b3c22
Spencer Archibald <spicyspice.dev@gmail.com>
2025-05-26 17:57:35 -0500

    lualine initial rewrite

--- nvim/lua/plugins/ui/lualine.lua
+++ nvim/lua/plugins/ui/lualine.lua
@@ -30,40 +30,46 @@
 --     c = { fg = colors.white },
 --   },
 -- }
-
 require('lualine').setup({
-  options = {
-    theme = 'spaceduck',
-    component_separators = '',
-    section_separators = { left = '', right = '' },
-  },
-  sections = {
-    lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
-    lualine_b = { 'filename', 'branch' },
-    lualine_c = {
-      '%=', --[[ add your center compoentnts here in place of this comment ]]
+    options = {
+        section_separators = { left = '', right = '' },
+        component_separators = '|',
+        theme = 'spaceduck'
     },
-    lualine_x = {
-        {
-        -- https://github.com/folke/noice.nvim/wiki/Configuration-Recipes#show-recording-messages
-            require("noice").api.statusline.mode.get,
-            cond = require("noice").api.statusline.mode.has,
-            color = { fg = "#d183e8" },
-        }
-    },
-    lualine_y = { 'filetype', 'progress' },
-    lualine_z = {
-      { 'location', separator = { right = '' }, left_padding = 2 },
-    },
-  },
-  inactive_sections = {
-    lualine_a = { 'filename' },
-    lualine_b = {},
-    lualine_c = {},
-    lualine_x = {},
-    lualine_y = {},
-    lualine_z = { 'location' },
-  },
-  tabline = {},
-  extensions = {},
 })
+-- require('lualine').setup({
+--   options = {
+--     theme = 'spaceduck',
+--     component_separators = '',
+--     section_separators = { left = '', right = '' },
+--   },
+--   sections = {
+--     lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
+--     lualine_b = { 'filename', 'branch' },
+--     lualine_c = {
+--       '%=', --[[ add your center compoentnts here in place of this comment ]]
+--     },
+--     lualine_x = {
+--         {
+--         -- https://github.com/folke/noice.nvim/wiki/Configuration-Recipes#show-recording-messages
+--             require("noice").api.statusline.mode.get,
+--             cond = require("noice").api.statusline.mode.has,
+--             color = { fg = "#d183e8" },
+--         }
+--     },
+--     lualine_y = { 'filetype', 'progress' },
+--     lualine_z = {
+--       { 'location', separator = { right = '' }, left_padding = 2 },
+--     },
+--   },
+--   -- inactive_sections = {
+--   --   lualine_a = { 'filename' },
+--   --   lualine_b = {},
+--   --   lualine_c = {},
+--   --   lualine_x = {},
+--   --   lualine_y = {},
+--   --   lualine_z = { 'location' },
+--   -- },
+--   tabline = {},
+--   extensions = {},
+-- })
